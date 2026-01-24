90b07b9c392d5dd6f1f9119b771eae2d746bedf6
Jeg <joelescanoguzman@gmail.com>
2026-01-24 17:06:33 -0400

    Crea Modelo de entidad Asignatura

---
+++ JEGRegistroEstudiante/Models/Asignaturas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JEGRegistroEstudiante.Models
+{
+    public class Asignaturas
+    {
+        [Key]
+        public int AsignaturaId { get; set; }
+
+        [Required(ErrorMessage = "El código es obligatorio")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El aula es obligatoria")]
+        public string Aula { get; set; }
+
+        [Required(ErrorMessage = "Los créditos son obligatorios")]
+        public int Creditos { get; set; }
+    }
+}
