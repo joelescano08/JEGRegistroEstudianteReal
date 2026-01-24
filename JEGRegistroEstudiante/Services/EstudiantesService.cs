f1500595419e17ed271e68c2c031eeeab7187ace
Jeg <joelescanoguzman@gmail.com>
2026-01-24 16:29:16 -0400

    Termina Componentes razor para la UI de la web

--- JEGRegistroEstudiante/Services/EstudiantesService.cs
+++ JEGRegistroEstudiante/Services/EstudiantesService.cs
@@ -16,7 +16,6 @@ namespace JEGRegistroEstudiante.Services
             return await contexto.Estudiantes.AnyAsync( estudiante => estudiante.EstudianteId == estudianteID );
 
         }
-
         private async Task<bool> Insertar(Estudiantes estudiante)
         {
             await using var contexto = await DbFactory.CreateDbContextAsync();
@@ -25,15 +24,13 @@ namespace JEGRegistroEstudiante.Services
 
 
         }
-
-        private async Task<bool> Modificar(Estudiantes estudiante)
+        public async Task<bool> Modificar(Estudiantes estudiante)
         {
             await using var contexto = await DbFactory.CreateDbContextAsync();
             contexto.Estudiantes.Update(estudiante);
 
             return await contexto.SaveChangesAsync() > 0;
         }
-
         public async Task<bool> Guardar(Estudiantes estudiante)
         {
             if(!await Existe(estudiante.EstudianteId)){
@@ -45,9 +42,6 @@ namespace JEGRegistroEstudiante.Services
             }
 
         }
-        
-        
-        
         public async Task<Estudiantes?> Buscar(int estudianteID)
         {
             await using var contexto = await DbFactory.CreateDbContextAsync();
@@ -55,24 +49,16 @@ namespace JEGRegistroEstudiante.Services
             return await contexto.Estudiantes.FirstOrDefaultAsync(estudiante => estudiante.EstudianteId == estudianteID);
 
         }
-
-
         public async Task<bool> Eliminar(int estudianteID)
         {
             await using var contexto = await DbFactory.CreateDbContextAsync();
 
             return await contexto.Estudiantes.AsNoTracking().Where(estudiante => estudiante.EstudianteId == estudianteID).ExecuteDeleteAsync() > 0;
         }
-
         public async Task<List<Estudiantes>> Listar(Expression<Func<Estudiantes, bool>> criterio){
             await using var contexto = await DbFactory.CreateDbContextAsync();
 
             return await contexto.Estudiantes.Where(criterio).AsNoTracking().ToListAsync();
         }
-
-
-
-
-
     }
 }
