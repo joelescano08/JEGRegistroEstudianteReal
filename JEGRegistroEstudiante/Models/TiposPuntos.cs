298b807ea7f4078d4db36d6c43eb02374efe3939
Jeg <joelescanoguzman@gmail.com>
2026-01-31 17:20:59 -0400

    Crea modelo TiposPuntos

---
+++ JEGRegistroEstudiante/Models/TiposPuntos.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JEGRegistroEstudiante.Models
+{
+    public class TiposPuntos
+    {
+        [Key]
+        public int TipoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        public string Descripcion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El valor de puntos es obligatorio")]
+        public int ValorPuntos { get; set; }
+
+        [Required(ErrorMessage = "El color es obligatorio")]
+        public string Color { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El icono es obligatorio")]
+        public string Icono { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El estado es obligatorio")]
+        public bool Activo { get; set; }
+    }
+}
